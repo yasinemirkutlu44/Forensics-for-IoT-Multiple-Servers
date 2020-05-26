a40ea8a923cf9c9a56cf533b647df9457db14c38
yasinemirkutlu44 <yasinemirkutlu44@gmail.com>
2020-05-26 16:14:23 +0300

    bug fixed and overall tests have been performed

--- Forensic/DeviceInvestigation.xaml.cs
+++ Forensic/DeviceInvestigation.xaml.cs
@@ -23,6 +23,15 @@ namespace Forensic
         public DeviceInvestigation()
         {
             InitializeComponent();
+            txtFinalMessage.IsEnabled = false;
+            txtOwnerName.IsEnabled = false;
+            txtUserName.IsEnabled = false;
+            txtDeviceType.IsEnabled = false;
+            txtIMEINumber.IsEnabled = false;
+            txtIPAddress.IsEnabled = false;
+            txtSerialNumber.IsEnabled = false;
+            txtLatitude.IsEnabled = false;
+            txtLongitude.IsEnabled = false;
         }
 
         IFirebaseConfig config = new FirebaseConfig()
@@ -150,8 +159,10 @@ namespace Forensic
         private void btnTraceDevices_Click(object sender, RoutedEventArgs e)
         {
             string concat = countryCode + cityCode+txtDeviceID.Text;
-            string[] result = System.IO.File.ReadAllLines(@"C:\Users\YasinEmir\result.txt");
+            string[] result = System.IO.File.ReadAllLines(@"C:\Users\YasinEmir\IoTDevices\" + concat + ".txt");
             firebaseClient = new FireSharp.FirebaseClient(config);
+            FirebaseResponse firebaseResponse = firebaseClient.Get(cmbCountries.SelectedItem.ToString() + "/" + cmbCities.SelectedItem.ToString() + "/" + concat);
+            Device responseDevice = firebaseResponse.ResultAs<Device>();
             if (String.IsNullOrWhiteSpace(cmbCountries.Text) || String.IsNullOrWhiteSpace(cmbCities.Text) || String.IsNullOrWhiteSpace(txtDeviceID.Text))
             {
                 MessageBox.Show("A required field was not filled", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
@@ -163,11 +174,28 @@ namespace Forensic
                     if (result[i] == "1.0 0.0")
                     {
                         txtFinalMessage.Text = "The picture which may pose a thread for crime! This IoT device should be investigated urgently!";
-
+                        txtOwnerName.Text = responseDevice.Owner;
+                        txtUserName.Text = responseDevice.User;
+                        txtIMEINumber.Text = responseDevice.IMEI;
+                        txtIPAddress.Text = responseDevice.IPAddress;
+                        txtSerialNumber.Text = responseDevice.SerialNumber;
+                        txtDeviceType.Text = responseDevice.DeviceType;
+                        txtLatitude.Text = responseDevice.Latitude.ToString();
+                        txtLongitude.Text = responseDevice.Longitude.ToString();
+                        break;
                     }
                     else
-                    {
+                    { 
                         txtFinalMessage.Text = "There is nothing illegal";
+                        txtOwnerName.Clear();
+                        txtUserName.Clear();
+                        txtIMEINumber.Clear();
+                        txtSerialNumber.Clear();
+                        txtLatitude.Clear();
+                        txtLongitude.Clear();
+                        txtIPAddress.Clear();
+                        txtSerialNumber.Clear();
+                        txtDeviceType.Clear();
                     }
                 }
             }
